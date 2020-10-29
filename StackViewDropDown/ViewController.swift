f9808131ad5a39bd4f4e28a1424127031f5951ed
Mohamed osama <mohamed.osama9812@gmail.com>
2020-10-29 13:29:30 -0700

    Drop Menu

--- StackViewDropDown/ViewController.swift
+++ StackViewDropDown/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var citybuttons: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func handleSelection(_ sender: UIButton) {
+        
+        citybuttons.forEach { (button) in
+            UIView.animate(withDuration: 0.5) {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    
+    @IBAction func cityTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
